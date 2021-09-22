6e4f0587492e846578aeb4b0da57523af9e7ccb4
Marcela Pino <marcelampino2018@gmail.com>
2021-09-22 17:13:47 -0400

    Completed display image and added click event to button

--- js/script.js
+++ js/script.js
@@ -3,11 +3,39 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-// console.log("Hello!")
+// Grab data from API & convert to JS Obj
 
 const getImage = async function(){
   const res = await fetch("https://picsum.photos/v2/list?limit=100");
   const images = await res.json();
-  console.log(images);
+  // console.log(images);
+  selectRandomImage(images);
+
 };
-getImage();
+
+// Manipulate API data obtained, & already converted to JS Obj
+
+const selectRandomImage = function(images){
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const randomImage = images[randomIndex];
+  // console.log(randomIndex)
+  // console.log(randomImage)
+  displayImage(randomImage);
+
+};
+
+const displayImage = function(randomImage){
+  const author = randomImage["author"];
+  const imageAddress = randomImage["download_url"]
+  console.log(author)
+  authorSpan.innerText = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove("hide");
+
+};
+
+
+button.addEventListener("click", function(){
+  getImage();
+
+});
